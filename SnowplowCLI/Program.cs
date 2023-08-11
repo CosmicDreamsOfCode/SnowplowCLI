5db34296a6b548730566084534fe782178f3fdfc
August <faintlightswitch@gmail.com>
2023-08-11 04:42:39 +0100

    Add project files.

---
+++ SnowplowCLI/Program.cs
@@ -0,0 +1,50 @@
+﻿using SnowplowCLI;
+using System.IO;
+using System.Reflection;
+
+namespace SnowplowCLI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //string filePath = args[0];
+            //int index = args[0].LastIndexOf("\\");
+            string filePath = @"I:\SwitchDumps\MarioRabbids\romfs\moria\sdf\nx\data\sdf.sdftoc";
+            string fileDir = Path.GetFileName(filePath);
+            string exepath = Assembly.GetExecutingAssembly().Location;
+            exepath = Path.GetDirectoryName(exepath);
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File does not exist.");
+                return;
+            }
+
+            using (NativeReader reader = new (new FileStream(filePath, FileMode.Open)))
+            {
+                string idCheck = reader.ReadSizedString(4);
+                if (idCheck == "WEST")
+                {
+                    uint version = reader.ReadUInt();
+                    if (version != 0x16) //i only really know of this one rn
+                    {
+                        Console.WriteLine("Unsupported version " + version + " Expected 0x16.");
+                        return;
+                    }
+                    else
+                    {
+                        //we have an toc we support so lets read it
+                        SDFS SDFS = new SDFS();
+                        SDFS.Read(reader, version);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid SDFTOC. Are you sure the file exists?");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ SnowplowCLI/SDFS.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnowplowCLI
+{
+    public class SDFS
+    {
+        public uint DecompressedSize;
+        public uint DataOffset; //not in 0x16 but lets set it up anyway
+        public uint CompressedSize;
+        public uint idCount;
+        public uint ddsHeaderCount;
+
+        public void Read(NativeReader reader, uint version)
+        {
+            DecompressedSize = reader.ReadUInt();
+            if (version >= 0x17)
+                DataOffset = reader.ReadUInt();
+            CompressedSize = reader.ReadUInt();
+            reader.Position += 4; //pad
+            idCount = reader.ReadUInt();
+            ddsHeaderCount = reader.ReadUInt();
+            var startId = new ID(reader);
+
+            byte flag = reader.ReadByte();
+            if (flag != 0)
+            {
+                byte[] unk = reader.ReadBytes(0x140);
+            }
+
+            if (DataOffset != 0)
+            {
+                reader.Position = DataOffset + 0x51;
+
+                uint signature = reader.ReadUInt();
+                //decompress name block 
+                var endId = new ID(reader);
+            }
+            else
+            {
+                //todo
+            }
+        }
+    }
+
+    public class ID
+    {
+        public string ubisoft;
+        public byte[] data;
+        public string massive;
+
+        public ID(NativeReader reader)
+        {
+            ubisoft = reader.ReadNullTerminatedString();
+            data = reader.ReadBytes(0x20);
+            massive = reader.ReadNullTerminatedString();
+        }
+    }
+}
